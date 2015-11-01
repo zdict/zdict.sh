739f9150d355d0a38bb3ffab9d9301a968ff05a3
Cychih <michael66230@gmail.com>
2015-11-02 03:12:56 +0800

    allow every window can have a zdict pane, not session-unique

--- zdict.plugin.zsh
+++ zdict.plugin.zsh
@@ -1,11 +1,19 @@
 if [[ -n "$TMUX" ]]; then
+    function _extract_zdict_pane_id () {
+        # check if tmux environment $ZDICT_PANE_VAR exists
+        ret=$(tmux show-environment $ZDICT_PANE_VAR 2>/dev/null)
+        if [[ $? -eq 0 ]]; then
+            # yes it exists, parse it and export to env
+            export ZDICT_PANE_ID=$(echo $ret | cut -d '=' -f 2)
+        fi
+    }
+
     function _prepare_zdict_pane () {
-        ZDICT_PANE=$(tmux show-environment ZDICT_PANE 2>/dev/null)
-        if [[ -n $ZDICT_PANE ]]; then
+        _extract_zdict_pane_id
+        if [[ -n "$ZDICT_PANE_ID" ]]; then
             # zdict pane was exist
-            eval "export $(tmux show-environment ZDICT_PANE 2>/dev/null)"
             # now try to select it
-            if $(tmux select-pane -t $ZDICT_PANE 2>/dev/null); then
+            if $(tmux select-pane -t $ZDICT_PANE_ID 2>/dev/null); then
                 # select successed, good
                 tmux select-pane -t $TMUX_PANE
                 return
@@ -13,20 +21,23 @@ if [[ -n "$TMUX" ]]; then
         fi
 
         # no zdict window exists now, make one
-        tmux split-window -h 'tmux set-environment ZDICT_PANE $TMUX_PANE; tmux wait-for -S ZDICT; zdict'
+        tmux split-window -h "tmux set-environment $ZDICT_PANE_VAR \$TMUX_PANE;\
+                tmux wait-for -S $ZDICT_PANE_VAR;\
+                zdict"
         tmux select-pane -t $TMUX_PANE
-        tmux wait-for ZDICT
-        eval "export $(tmux show-environment ZDICT_PANE 2>/dev/null)"
-        tmux send-keys -t .$ZDICT_PANE C-l
+        tmux wait-for $ZDICT_PANE_VAR
+        _extract_zdict_pane_id
     }
 
     function _query_zdict () {
         if [[ -n "$BUFFER" ]]; then
             querying_word=$BUFFER
             BUFFER='Querying...'   # display a message for user experience
+            # tmux window index may change, so it cannot be cached
+            export ZDICT_PANE_VAR="ZDICT_PANE_$(tmux display-message -p '#I')"
             _prepare_zdict_pane
-            tmux send-keys -l -t .$ZDICT_PANE $querying_word
-            tmux send-keys -t .$ZDICT_PANE Enter
+            tmux send-keys -l -t .${ZDICT_PANE_ID} $querying_word
+            tmux send-keys -t .${ZDICT_PANE_ID} Enter
             zle .kill-whole-line
         fi
     }
